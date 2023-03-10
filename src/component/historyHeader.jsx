786fc8c42979e19cd4b55f23a01b7d06d2bd67fa
sanil011 <sanilp191@gmail.com>
2023-03-11 05:01:28 +0530

    last

--- src/component/historyHeader.jsx
+++ src/component/historyHeader.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import { Box, Button, Typography } from "@mui/material"
-import { Navigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 const HeaderHistory = () => {
 
-
+    const navigate = useNavigate();
     const handleClick = () => {
 
-       <Navigate to="/"/>
+       navigate('/')
 
     }
 
